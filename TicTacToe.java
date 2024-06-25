61f3c4f756435e7d28bb69215b5c7c20750a8e86
abdullahklc <wi23b136@technikum-wien.at>
2024-06-25 13:20:33 +0200

    Create TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public TicTacToe() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
+    }
+
+    public void start() {
+        while (!board.isFull() && !board.hasWinner()) {
+            board.print();
+            System.out.println("Current Player: " + currentPlayer.getMarker());
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.print("Enter column (0-2): ");
+            int col = scanner.nextInt();
+            if (board.isCellEmpty(row, col)) {
+                board.place(row, col, currentPlayer.getMarker());
+                if (board.hasWinner()) {
+                    board.print();
+                    System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+                    return;
+                }
+                switchCurrentPlayer();
+            } else {
+                System.out.println("Cell is not empty, try again.");
+            }
+        }
+        if (board.isFull() && !board.hasWinner()) {
+            board.print();
+            System.out.println("It's a draw!");
+        }
+    }
+
+    private void switchCurrentPlayer() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
+}
